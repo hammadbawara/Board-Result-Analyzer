a341f9433ed1aacaeb23aa1a9338318774e2daa9
Hammad Zafar Bawara <hammadbawara@gmail.com>
2022-09-02 00:01:26 +0500

    sqlite database added and saving in text file removed

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 # importing required modules
 import os
 import re
-
 import PyPDF2
 from termcolor import colored
 from tqdm import tqdm
+import sqlite3
 
 PDF_FILE_NAME = "MA2022 GAZETTE.pdf"
-TEXT_FILE_NAME = f'{PDF_FILE_NAME}.txt'
-INSTITUTES_LIST_FILE = f'{PDF_FILE_NAME}-Institutes-List.txt'
-
+DB_FILE_NAME = f'{PDF_FILE_NAME}.db'
 
 def create_text_file():
+
+    json_data = {}
+
     # creating a pdf file object
     pdfFileObj = open('MA2022 GAZETTE.pdf', 'rb')
 
@@ -23,41 +24,58 @@ def create_text_file():
     student_result_split_pattern = "([0-9]{6})([A-Z\s]*)([-]*)"
     institute_name_pattern = "[0-9]{6}-[A-Z., ()-]*"
 
-    with open(TEXT_FILE_NAME, "w") as f:
-        f.write("")
-    with open(INSTITUTES_LIST_FILE, "w") as f:
-        f.write("")
-
-    text_file = open(TEXT_FILE_NAME, "a")
-    institutes_text_file = open(INSTITUTES_LIST_FILE, "a")
 
     print(colored("EXTRACTING TEXT FROM PDF ..........", "green"))
+
+    institute_code = None
+
+    # Initializing database
+    sql = sqlite3.connect(DB_FILE_NAME)
+    cursor = sql.cursor()
+    cursor.execute("CREATE TABLE IF NOT EXISTS result(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, roll_num INTEGER, marks TEXT, institute INETEGER)")
+    cursor.execute("CREATE TABLE IF NOT EXISTS schools(id INTEGER PRIMARY KEY AUTOINCREMENT, school_code INTEGER, school_name)")
 
     for i in tqdm(range(number_of_pages)):
         pageObj = pdfReader.getPage(i)
         page_text = pageObj.extractText()
 
+        # creating lines list
         page_text_lines = page_text.split("\n")
 
         for i, line in enumerate(page_text_lines):
             line = line.strip()
             if re.match(student_result_pattern, line):
+                # when i == 41 then two students numbers are in one line
                 if i == 41:
+                    # seperating students
                     two_students = re.findall(student_result_pattern, line)
                     for student in two_students:
+                        # saving into database
                         result = re.split(student_result_split_pattern, student)
-                        text_file.write(f'{result[1]}\t\t{result[2]}\t\t{result[4]}\n')
+                        cursor.execute("INSERT INTO result(name, roll_num, marks, institute) VALUES(?,?,?,?)", 
+                        (result[2], result[1], result[4], int(institute_code)))
                 else:
+                    # Saving into database
                     result = re.split(student_result_split_pattern, line)
-                    text_file.write(f'{result[1]}\t\t{result[2]}\t\t{result[4]}\n')
+                    student = {"name":result[2], "roll num": result[1], "marks": result[4]}
+                    cursor.execute("INSERT INTO result(name, roll_num, marks, institute) VALUES(?,?,?,?)", 
+                    (result[2], result[1], result[4], institute_code))
+                    
             elif re.match(institute_name_pattern, line):
-                text_file.write(f'\n\n{line}\n')
-                institutes_text_file.write(f'{line}\n')
+                position = line.find("-")
+                institute_code = int(line[:position])
+                institude_name = line[position+1:]
+                cursor.execute("INSERT INTO schools(school_code, school_name) VALUES(?,?)",(institute_code, institude_name))            
+
+    # Commiting changes in database 
+    sql.commit()
+    cursor.close()
+    sql.close()
 
     # closing the pdf file object
     pdfFileObj.close()
 
 
 if __name__ == '__main__':
-    if not os.path.isfile(TEXT_FILE_NAME):
+    if not os.path.isfile(DB_FILE_NAME):
         create_text_file()
